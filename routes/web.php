4996527d0e3a517fe0b42bab8c986c958f410e25
naemulislamdev <naemulislam.dev@gmail.com>
2025-10-16 18:40:33 +0600

    add backend css

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,18 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Web\WebController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+
+    return "Cache cleared successfully!";
+});
 
 Route::get('/home', function () {
     return view('user-account');
